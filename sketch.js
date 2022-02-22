3964b8dce90d74d759eddc924c8df88e2eddd62c
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-02-22 15:39:56 -0500

    Added Comments

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+//How much it should rotate each iteration
 var radStep = Math.PI / 9;
+//How far away from the center should the ellipses be moved each iteration
 var distanceStep = 1;
+//Total count of the ellipses
 var count = 200;
 
 function setup() {
@@ -12,14 +15,15 @@ function draw() {
   background(220);
   fill(255);
 
-  // for(var x = 0; x < width; x+=75) {
-  //   // ellipse(x,height/2,50,50);
-  // }
   for(var i = 0; i < count; i++) {
+    //Set the fill to start as black and approach white
     fill(i / count * 255);
+    //set x and y to gradually ratiate and grow away from the center
     var x = width / 2 + Math.cos(radStep * i) * distanceStep * i;
     var y = height / 2 + Math.sin(radStep * i) * distanceStep * i;
+    //Sets thes size to shrink as the circle gets further away
     var size = (count-i) * count / count;
+    //Draw the ellipse
     ellipse(x,y,size,size);
   }
 }
